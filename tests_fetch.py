d083765672ae3eaa3efb198cd7a5bbe640c624f6
AvangardAA <artemnzr@gmail.com>
2023-10-03 18:29:25 +0300

    conerror, empty, timeout, formaterror

--- tests_fetch.py
+++ tests_fetch.py
@@ -1,29 +1,39 @@
 import unittest
 from unittest.mock import patch, Mock
+import requests
+from algo import fetch
 
 class TestFetchFunction(unittest.TestCase):
     @patch('requests.get')
-    def test_fetch_200(self, mockres):
+    def test_fetch_200_empty(self, mockres):
         mock_response = Mock()
         mock_response.status_code = 200
-        mock_response.json.return_value = {"data": ""}
+        mock_response.json.return_value = {"data": []}
         mockres.return_value = mock_response
-        self.assertEqual(mock_response.status_code, 200)
+        res = fetch(0)
+        self.assertEqual(res, {"data": []})
     @patch('requests.get')
     def test_fetch_500(self, mockres):
         mock_response = Mock()
         mock_response.status_code = 500
         mockres.return_value = mock_response
-        self.assertEqual(mock_response.status_code, 500)
+        res = fetch(0)
+        self.assertEqual(res, [])
     @patch('requests.get')
-    def test_fetch_403(self, mockres):
-        mock_response = Mock()
-        mock_response.status_code = 403
-        mockres.return_value = mock_response
-        self.assertEqual(mock_response.status_code, 403)
+    def test_fetch_conerror(self, mockres):
+        mockres.side_effect = requests.exceptions.RequestException("http connection error")
+        res = fetch(0)
+        self.assertEqual(res, [])
     @patch('requests.get')
-    def test_fetch_404(self, mockres):
+    def test_fetch_timeout(self, mockres):
+        mockres.side_effect = requests.exceptions.RequestException("timeout")
+        res = fetch(0)
+        self.assertEqual(res, [])
+    @patch('requests.get')
+    def test_fetch_formaterror(self, mockres):
         mock_response = Mock()
-        mock_response.status_code = 404
+        mock_response.status_code = 200
+        mock_response.text = "0"
         mockres.return_value = mock_response
-        self.assertEqual(mock_response.status_code, 404)
+        res = fetch(0)
+        self.assertEqual(res, [])
