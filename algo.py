10d734c58c918b7c978dd8571e7f2157f4f4c434
AvangardAA <artemnzr@gmail.com>
2023-10-12 01:57:51 +0300

    +fastapi for requests and reworked better implementation of timenorm

--- algo.py
+++ algo.py
@@ -1,8 +1,10 @@
+from fastapi import FastAPI
 import requests
-from datetime import datetime, timedelta
-api_url = "https://sef.podkolzin.consulting/api/users/lastSeen"
+from datetime import datetime
+api_url = "https://sef.podkolzin.consulting"
+app = FastAPI()
 def fetch(offset):
-    response = requests.get(f"{api_url}?offset={offset}")
+    response = requests.get(f"{api_url}/api/users/lastSeen?offset={offset}")
     if response.status_code == 200:
         return response.json()
     else:
@@ -38,42 +40,163 @@ dictionary = \
     }
 }
 
-def normalize_time(last_seen_date, locale):
+def normalize_time_human_implementation(last_seen_date, locale, DDate=None):
     if not last_seen_date:
         return dictionary[locale]['thxfor3amcoding']
 
-    now = datetime.utcnow()
+    ts_now = int(datetime.now().timestamp())
+    tsDDate = 0
+    ts = 1
 
-    date_str = last_seen_date[:10]
-    time_str = last_seen_date[11:19]
-
-    fracsec = 0
-    if '.' in last_seen_date:
-        fracsecstr = last_seen_date.split('.')[1]
-        if fracsecstr.isdigit():
-            fracsec = int(fracsecstr)
+    if DDate is not None:
+        try:
+            DDate=transform_to_iso_format(DDate)
+            tsDDate = int(datetime.fromisoformat(DDate).timestamp())
+        except:
+            return dictionary[locale]['unreal_to_parse']
 
     try:
-        date_time = datetime.strptime(date_str + ' ' + time_str, "%Y-%m-%d %H:%M:%S")
-        date_time += timedelta(microseconds=fracsec)
-    except ValueError:
+        ts = int(datetime.fromisoformat(last_seen_date).timestamp())
+    except:
         return dictionary[locale]['unreal_to_parse']
 
-    difference = now - date_time
-
-    if difference < timedelta(seconds=30):
-        return dictionary[locale]['now']
-    elif difference < timedelta(seconds=60):
-        return dictionary[locale]['last_min']
-    elif difference < timedelta(minutes=59):
-        return dictionary[locale]['less_than_hour_minutes']
-    elif difference < timedelta(minutes=119):
-        return dictionary[locale]['hour_ago']
-    elif difference < timedelta(hours=24):
-        return dictionary[locale]['today']
-    elif timedelta(hours=24):
-        return dictionary[locale]['yesterday']
-    elif difference < timedelta(days=7):
-        return dictionary[locale]['week']
+    if DDate is not None:
+        #print(abs(tsDDate - ts))
+        if abs(tsDDate-ts) < 86399:
+            return {"ts": ts, "status": "yes"}
+        else:
+            return {"ts": abs(tsDDate - ts), "status": "no"}
     else:
-        return dictionary[locale]['sleeping']
+        if ts_now-ts < 30:
+            return dictionary[locale]['now']
+        elif 60 > ts_now-ts > 30:
+            return dictionary[locale]['last_min']
+        elif 3599 > ts_now-ts > 60:
+            return dictionary[locale]['less_than_hour_minutes']
+        elif 7198 > ts_now-ts > 3599:
+            return dictionary[locale]['hour_ago']
+        elif 86399 > ts_now-ts > 7198:
+            return dictionary[locale]['today']
+        elif 172799 > ts_now-ts > 86399:
+            return dictionary[locale]['yesterday']
+        elif 604799 > ts_now-ts > 172799:
+            return dictionary[locale]['week']
+        else:
+            return dictionary[locale]['sleeping']
+
+@app.get('/api/stats/users')
+async def get_users_historical_data(date: str):
+    if not date:
+        return {"usersOnline": None}
+
+    c = 0
+    cc = 0
+    users_data = fetch(c)
+    q = int(users_data['total'])
+    while q > 0:
+        for user in users_data['data']:
+            res = normalize_time_human_implementation(user['lastSeenDate'], "english", DDate=date)
+            try:
+                if res["status"]=="yes":
+                    cc += 1
+            except:
+                pass
+        q -= len(users_data['data'])
+        c += len(users_data['data'])
+        users_data = fetch(c)
+
+    return {"usersOnline": cc}
+
+@app.get('/api/stats/user')
+async def get_user_historical_data(date: str, userId: str):
+    if not date or not userId:
+        return {"wasUserOnline": None, "nearestOnlineTime": None}
+
+    c = 0
+    was = False
+    users_data = fetch(c)
+    q = int(users_data['total'])
+    nearest = None
+    while q > 0:
+        for user in users_data['data']:
+            if user["userId"] == userId.lower():
+                res = normalize_time_human_implementation(user['lastSeenDate'], "english", DDate=date)
+                if (res=='0'):
+                    was = None
+                    break
+                try:
+                    if res["status"] == "yes":
+                        was = True
+                        break
+                    else:
+                        nearest = int(res["ts"])
+                        if nearest is not None:
+                            if nearest < res["ts"]:
+                                nearest = res["ts"]
+                except:
+                    pass
+        q -= len(users_data['data'])
+        c += len(users_data['data'])
+        users_data = fetch(c)
+
+    return {"wasUserOnline": was, "nearestOnlineTime": nearest}
+
+@app.get('/api/predictions/users')
+async def predict_users_online(date: str):
+    if not date:
+        return {"OnlineUsers": None}
+
+    c = 0
+    cc = 0
+    users_data = fetch(c)
+    q = int(users_data['total'])
+    while q > 0:
+        for user in users_data['data']:
+            res = normalize_time_human_implementation(user['lastSeenDate'], "english", DDate=transform_from_iso_format(int(datetime.now().timestamp())+10800))
+            try:
+                if res["status"]=="yes":
+                    cc += 1
+            except:
+                pass
+        q -= len(users_data['data'])
+        c += len(users_data['data'])
+        users_data = fetch(c)
+
+    if check_dates_correspond(date, transform_from_iso_format(int(datetime.now().timestamp())+10800)):
+        return {"OnlineUsers": cc}
+    else:
+        return {"Error": "datesArentSame"}
+
+@app.get('/api/predictions/user')
+async def predict_user_online(date: str, tolerance: float, userId: str):
+    if not date or not userId:
+        return {"willBeOnline": None, "onlineChance": None}
+
+    c = 0
+    was = False
+    users_data = fetch(c)
+    q = int(users_data['total'])
+    chance = 0.0
+    while q > 0:
+        for user in users_data['data']:
+            if user["userId"] == userId.lower():
+                res = normalize_time_human_implementation(user['lastSeenDate'], "english", DDate=transform_from_iso_format(int(datetime.now().timestamp())+10800))
+                if (res == '0'):
+                    was = None
+                    break
+                try:
+                    if res["status"] == "yes":
+                        was = True
+                        break
+                except:
+                    pass
+        q -= len(users_data['data'])
+        c += len(users_data['data'])
+        users_data = fetch(c)
+
+    if (was==True):
+        chance = float(1 / ((int(datetime.fromisoformat(transform_to_iso_format(date)).timestamp())-(int(datetime.now().timestamp())+10800))/604800)*100)
+
+    if chance < tolerance:
+        was = False
+    return {"willBeOnline": was, "onlineChance": "{:.2f}".format(chance)}
